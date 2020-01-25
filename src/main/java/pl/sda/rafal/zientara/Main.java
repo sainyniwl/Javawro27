20446b81a6dd46abcbd1dbb250fbc9e179fe0d52
Rafal Zientara <rafal.zientara92@gmail.com>
2020-01-25 11:56:53 +0100

    start of tic tac toe

--- src/main/java/pl/sda/rafal/zientara/Main.java
+++ src/main/java/pl/sda/rafal/zientara/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Siemanko!");
+
+        Runnable run = () -> System.out.println("lol");
+
+        // given
+
+        //when
+
+        //then
     }
 }
