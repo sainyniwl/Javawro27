d15108b888428ac93183becd8704e8a74671cec9
Konrad Brylak <konrad.brk@gmail.com>
2020-01-25 15:55:53 +0100

    TicTacToe Window implementation

--- src/main/java/pl/sda/rafal/zientara/TicTacToeWindow.java
+++ src/main/java/pl/sda/rafal/zientara/TicTacToeWindow.java
@@ -1,45 +1,49 @@
-package pl.sda.rafal.zientara.swing;
+package pl.sda.rafal.zientara;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class FirstWindow {
+public class TicTacToeWindow {
     private JFrame frame;
-    private JButton button;
     private JLabel counter;
     private int counterClick;
+    private TicTacToe game = new TicTacToe();
 
 
     public static void main(String[] args) {
-        new FirstWindow();
+        new TicTacToeWindow();
     }
 
-    public FirstWindow() {
+    public TicTacToeWindow() {
         frame = new JFrame("Cuś");
         frame.setSize(300, 350);
-
-        button = new JButton("Wincyj!");
-        button.setBounds(50, 50, 200, 100);
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                counterClick++;
-                String text = Integer.toString(counterClick);
-                counter.setText(text);
-            }
-        });
-        frame.add(button);
-
         frame.setResizable(false);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        counter = new JLabel("0", SwingConstants.CENTER);
-        counter.setBounds(50, 200, 200, 100);
-        frame.add(counter);
+        GridLayout experimentLayout = new GridLayout(3, 3);
 
-        frame.setLayout(null);
+        for (int i = 0; i < 9; i++) {
+            int col = i % TicTacToe.BOARD_SIZE;
+            int row = i / TicTacToe.BOARD_SIZE;
+            JButton button = new JButton();
+            button.setBounds(50, 50, 200, 100);
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println("col " + col);
+                    System.out.println("row " + row);
+                    game.action(col, row);
+                    FieldStatus status = game.getFieldStatus(col, row);
+                    button.setText(status.getText());
+                    frame.setTitle(game.checkResult().toString());
+                }
+            });
+            frame.add(button);
+        }
+
+        frame.setLayout(experimentLayout);
         frame.setVisible(true);
     }
 }
