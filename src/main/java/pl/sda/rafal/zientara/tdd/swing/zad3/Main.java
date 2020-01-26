a9eede370b0eec4d54a386f942e38c3e7393a5c9
Rafal Zientara <rafal.zientara92@gmail.com>
2020-01-26 11:03:08 +0100

    secret windows

---
+++ src/main/java/pl/sda/rafal/zientara/tdd/swing/zad3/Main.java
@@ -0,0 +1,14 @@
+package pl.sda.rafal.zientara.tdd.swing.zad3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LoginWindow loginWindow = new LoginWindow();
+        loginWindow.setListener(new LoginWindow.LoginListener() {
+            @Override
+            public void onLoginClicked(String password) {
+                System.out.println(password);
+            }
+        });
+    }
+}
