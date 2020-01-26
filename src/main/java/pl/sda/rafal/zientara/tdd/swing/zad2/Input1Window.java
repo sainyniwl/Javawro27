bb98358c6fd815e3347ee2792b8a6d6ae0821acf
Rafal Zientara <rafal.zientara92@gmail.com>
2020-01-26 10:33:44 +0100

    Text merger

--- src/main/java/pl/sda/rafal/zientara/tdd/swing/zad2/Input1Window.java
+++ src/main/java/pl/sda/rafal/zientara/tdd/swing/zad2/Input1Window.java
@@ -3,7 +3,7 @@ package pl.sda.rafal.zientara.tdd.swing.zad2;
 import javax.swing.*;
 
 public class Input1Window {
-    private JTextField textView;
+    private JTextField textView1;
     private JLabel label;
 
     public Input1Window(){
@@ -12,9 +12,9 @@ public class Input1Window {
         frame.setResizable(false);
         frame.setSize(300,350);
 
-        textView = new JTextField("input 1 text");
-        textView.setBounds(50,50, 200,100);
-        frame.add(textView);
+        textView1 = new JTextField("input 1 text");
+        textView1.setBounds(50,50, 200,100);
+        frame.add(textView1);
 
         label = new JLabel("0", SwingConstants.CENTER);
         label.setBounds(50,200,200,100);
@@ -22,5 +22,13 @@ public class Input1Window {
 
         frame.setLayout(null);
         frame.setVisible(true);
+    }
+
+    public String getText1() {
+        return textView1.getText();
+    }
+
+    public void setMergedText(String text) {
+        label.setText(text);
     }
 }
