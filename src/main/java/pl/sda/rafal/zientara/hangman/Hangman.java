d03d002c2fe126e820c14842eed058e6e899e3ec
Maciej Nawojczyk <macieekn@gmail.com>
2020-01-26 12:59:30 +0100

    Commit with 8 new tests in Hangman game

--- src/main/java/pl/sda/rafal/zientara/hangman/Hangman.java
+++ src/main/java/pl/sda/rafal/zientara/hangman/Hangman.java
@@ -1,9 +1,14 @@
 package pl.sda.rafal.zientara.hangman;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Hangman {
 
 
     private String puzzle;
+    private Set<Character> guessedLetter = new LinkedHashSet<>();
+    private int hp = 7;
 
     public void setPuzzle(String puzzle) {
         this.puzzle = puzzle;
@@ -13,12 +18,33 @@ public class Hangman {
         StringBuilder output = new StringBuilder();
         for (int i = 0; i < puzzle.length(); i++) {
             char c = puzzle.charAt(i);
-                if(Character.isWhitespace(c)){
-                    output.append(" ");
-                } else{
+            if (Character.isWhitespace(c)) {
+                output.append(" ");
+            } else {
+                if (guessedLetter.contains(Character.toLowerCase(c))) {
+                    output.append(c);
+                } else {
                     output.append(".");
                 }
+            }
         }
         return output.toString();
     }
+
+
+    public void guessLetter(char c) {
+        char smallChar = Character.toLowerCase(c);
+        guessedLetter.add(smallChar);
+        boolean isCorrect = puzzle.toLowerCase().contains(Character.toString(smallChar));
+
+        if(!isCorrect){
+            hp--;
+        }
+    }
+
+
+    public int getHp() {
+        return hp;
+    }
+
 }
--- src/test/java/pl/sda/rafal/zientara/hangman/HangmanTest.java
+++ src/test/java/pl/sda/rafal/zientara/hangman/HangmanTest.java
@@ -1,15 +1,22 @@
 package pl.sda.rafal.zientara.hangman;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HangmanTest {
 
+    private Hangman game;
+
+    @BeforeEach
+    public void setup(){
+        game = new Hangman();
+        game.setPuzzle("Ala ma kota");
+    }
+
     @Test
     public void outputShouldBeEncrypted() {
-        Hangman game = new Hangman();
-        game.setPuzzle("Ala ma kota");
 
         //when
         String output = game.getOutput();
@@ -18,5 +25,110 @@ class HangmanTest {
         assertEquals("... .. ....", output);
 
     }
+
+    @Test
+    public void correctLetterShouldBeVisible(){
+        //given
+        game.guessLetter('M');
+
+        //when
+        String output = game.getOutput();
+
+        //then
+        assertEquals("... m. ....", output);
+
+
+    }
+
+    @Test
+    public void correctLetterShouldBeVisible_2(){
+        //given
+        game.guessLetter('a');
+
+        //when
+        String output = game.getOutput();
+
+        //then
+        assertEquals("A.a .a ...a", output);
+
+
+    }
+
+    @Test
+    public void correctTwoLetterShouldBeVisible(){
+        //given
+        game.guessLetter('a');
+        game.guessLetter('m');
+
+        //when
+        String output = game.getOutput();
+
+        //then
+        assertEquals("A.a ma ...a", output);
+
+
+    }
+
+    @Test
+    public void fullOutputShouldBeVisible(){
+        //given
+        game.guessLetter('a');
+        game.guessLetter('m');
+        game.guessLetter('l');
+        game.guessLetter('o');
+        game.guessLetter('k');
+        game.guessLetter('t');
+
+        //when
+        String output = game.getOutput();
+
+        //then
+        assertEquals("Ala ma kota", output);
+
+
+    }
+
+    @Test
+    public void wrongGuessCauseDamage(){
+        //given
+
+        //when
+        game.guessLetter('x');
+
+        //then
+        int hp = game.getHp();
+        assertEquals(6, hp);
+    }
+
+    @Test
+    public void playerShouldHave7HpOnStart(){
+        //given
+
+        //when
+
+        //then
+        int hp = game.getHp();
+        assertEquals(7, hp);
+    }
+
+    @Test
+    public void wrongGuessesWillKillYou(){
+        //given
+
+        //when
+        game.guessLetter('C');
+        game.guessLetter('D');
+        game.guessLetter('E');
+        game.guessLetter('F');
+        game.guessLetter('G');
+        game.guessLetter('H');
+        game.guessLetter('I');
+
+        //then
+        int hp = game.getHp();
+        assertEquals(0, hp);
+    }
+
+    
 
 }
