348246f69d9ed3508f3a47b2c622efcacf2aa8bb
PawelSiniecki <sainyniwl@gmail.com>
2020-02-15 11:10:28 +0100

    FX Calculator View

---
+++ src/main/java/pl/sda/rafal/zientara/tdd/calculator/CalcJavaFx.java
@@ -0,0 +1,23 @@
+package pl.sda.rafal.zientara.tdd.calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CalcJavaFx extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass()
+                .getResource("/calc.fxml"));
+        primaryStage.setTitle("Calc");
+        Scene scene = new Scene(root, primaryStage.getWidth(),
+                primaryStage.getHeight());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
