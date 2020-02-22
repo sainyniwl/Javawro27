f9cfabefd525cedb6d53e43682ae776217b564bc
PawelSiniecki <sainyniwl@gmail.com>
2020-02-22 15:57:42 +0100

    File remover.

--- src/main/java/pl/sda/rafal/zientara/tdd/File/FileMain.java
+++ src/main/java/pl/sda/rafal/zientara/tdd/File/FileMain.java
@@ -1,6 +1,11 @@
 package pl.sda.rafal.zientara.tdd.File;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,15 +26,10 @@ public class FileMain {
 
         addSubFiles(file, allFiles);
 
-        //printFilteredFiles(allFiles);
         System.out.println("===================== FINAL RESULT =====================");
         for (File f : allFiles) {
             System.out.println(f);
         }
-        //List<File> pngFiles = allFiles.stream()
-//                .filter(fil -> fil.getName().endsWith(".png"))
-//                .collect(Collectors.toList());
-
         killDuplicates(allFiles);
     }
 
@@ -38,6 +38,7 @@ public class FileMain {
             System.out.println("Checking duplicates for " + png);
             List<File> duplicates = getDuplicates(png, allFiles);
             System.out.println("Duplicates:" + duplicates);
+            //TODO znajdż inny sposób na usuwanie powtórnego sprawdzenia
             deleteDuplicates(duplicates);
         }
     }
@@ -71,10 +72,44 @@ public class FileMain {
 
         if (png.length() == possibleDuplicate.length()) {
             //TODO porównaj pliki bajt po bajcie
-            return true;
+            return comapreByBytes(png, possibleDuplicate);
         } else {
             return false;
         }
+    }
+
+    private static boolean comapreByBytes(File png, File possibleDuplicate) {
+        try {
+            FileReader reader1 = new FileReader(png);
+            FileReader reader2 = new FileReader(possibleDuplicate);
+
+            int read1;
+            int read2;
+
+            do {
+                read1 = reader1.read();
+                read2 = reader2.read();
+
+                if (read1 != read2) {
+                    reader1.close();
+                    reader2.close();
+                    return false;
+                }
+
+            } while (read1 != -1 && read2 != -1);
+
+            reader1.close();
+            reader2.close();
+            return true;
+        } catch (FileNotFoundException e) {
+            System.out.println("Nie ma takiego pliku!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Błąd odczytu!");
+            e.printStackTrace();
+        }
+
+        return false;
     }
 
     private static void printFilteredFiles(List<File> allFiles) {
