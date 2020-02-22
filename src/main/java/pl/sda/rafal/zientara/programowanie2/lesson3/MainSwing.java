593b474914c029b613cc594b4cf47f62057bc579
Rafal Zientara <rafal.zientara92@gmail.com>
2020-02-22 15:25:04 +0100

    file killer

---
+++ src/main/java/pl/sda/rafal/zientara/programowanie2/lesson3/MainSwing.java
@@ -0,0 +1,13 @@
+package pl.sda.rafal.zientara.programowanie2.lesson3;
+
+import javax.swing.*;
+
+public class MainSwing {
+
+    public static void main(String[] args) {
+        System.out.println("siema z konsoli");
+        JFrame frame = new JFrame("Nowy jar");
+        frame.setSize(400,300);
+        frame.setVisible(true);
+    }
+}
