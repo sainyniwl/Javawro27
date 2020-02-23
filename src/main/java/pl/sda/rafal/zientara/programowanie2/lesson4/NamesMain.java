db099ec5cba13d153ec653a8668c26b263c222b7
Rafal Zientara <rafal.zientara92@gmail.com>
2020-02-23 11:46:13 +0100

    letters comparator

--- src/main/java/pl/sda/rafal/zientara/programowanie2/lesson4/NamesMain.java
+++ src/main/java/pl/sda/rafal/zientara/programowanie2/lesson4/NamesMain.java
@@ -6,10 +6,67 @@ public class NamesMain {
 
     public static void main(String[] args) {
         System.out.println(new File("").getAbsolutePath());
-        String fileName = "maleNames.txt";
-        String[] maleNames = getNamesFromFile(fileName);
+        String[] maleNames = getNamesFromFile("maleNames.txt");
+        String[] femaleNames = getNamesFromFile("femaleNames.txt");
 
+//        Circle.class.getSimpleName()
+        String report = getResultReport(maleNames, femaleNames);
+        saveReportToFile(report);
+    }
 
+    private static void saveReportToFile(String report) {
+        File file = new File("report.txt");
+        try {
+            FileWriter writer = new FileWriter(file);
+            writer.write(report);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        openTextEditor(file);
+    }
+
+    private static void openTextEditor(File file) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("cmd.exe", "/c", file.getAbsolutePath());
+        try {
+            processBuilder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getResultReport(String[] maleNames, String[] femaleNames) {
+        StringBuilder builder = new StringBuilder();
+        for (char i = 'A'; i <= 'Z'; i++) {
+            char currentLetter = i;
+            int maleCount = getNamesWithLetterCount(currentLetter, maleNames);
+            int femaleCount = getNamesWithLetterCount(currentLetter, femaleNames);
+            if (maleCount > femaleCount) {
+                builder.append("Wiêcej mêskich imion na literê ").append(currentLetter);
+            } else if (maleCount < femaleCount) {
+                builder.append("Wiêcej ¿eñskich imion na literê ").append(currentLetter);
+            } else {
+                builder.append("Tyle samo imion na literê ").append(currentLetter);
+            }
+            if (i < 'Z')
+                builder.append(System.lineSeparator());
+
+        }
+        System.out.println(builder);
+        return builder.toString();
+    }
+
+    private static int getNamesWithLetterCount(char firstLetter, String[] names) {
+        int count = 0;
+//        String firstChar = String.valueOf(firstLetter);//sposob 1
+        for (String name : names) {
+//            if(name.startsWith(firstChar)) {//sposob 1
+            if (name.charAt(0) == firstLetter) {//sposob 2
+                count++;
+            }
+        }
+        return count;
     }
 
     private static String[] getNamesFromFile(String fileName) {
@@ -34,13 +91,12 @@ public class NamesMain {
     }
 
     private static String[] getNormalizedNames(String allNames) {
-        String[] names;
-        names = allNames.split(",");
+        String[] names = allNames.split(",");
 
         for (int i = 0; i < names.length; i++) {
             String name = names[i];
-            name = name.trim();
-            System.out.println("#" + name + "#");
+            name = name.trim();//"      Krzyœ " -> " Krzyœ"
+//            System.out.println("#" + name + "#");
             names[i] = name;
         }
         return names;
